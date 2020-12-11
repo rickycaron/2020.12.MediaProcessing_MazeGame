eb2bb3442e483985fb7b7b4752d24869ce4fe1cc
rui.zhu <rui.zhu@student.kuleuven.be>
2020-12-11 13:47:33 +0100

    build in linux

---
+++ TASKB/build-taskb-Desktop_Qt_5_15_1_GCC_64bit-Debug/moc_controller.cpp
@@ -0,0 +1,138 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'controller.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../taskb/controller.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'controller.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_Controller_t {
+    QByteArrayData data[9];
+    char stringdata0[83];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Controller_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Controller_t qt_meta_stringdata_Controller = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "Controller"
+QT_MOC_LITERAL(1, 11, 4), // "move"
+QT_MOC_LITERAL(2, 16, 0), // ""
+QT_MOC_LITERAL(3, 17, 14), // "getPoisonLevel"
+QT_MOC_LITERAL(4, 32, 1), // "p"
+QT_MOC_LITERAL(5, 34, 16), // "resetPoisonLevel"
+QT_MOC_LITERAL(6, 51, 11), // "getPoisoned"
+QT_MOC_LITERAL(7, 63, 8), // "gameover"
+QT_MOC_LITERAL(8, 72, 10) // "checkvalue"
+
+    },
+    "Controller\0move\0\0getPoisonLevel\0p\0"
+    "resetPoisonLevel\0getPoisoned\0gameover\0"
+    "checkvalue"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Controller[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       5,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   39,    2, 0x08 /* Private */,
+       3,    1,   40,    2, 0x08 /* Private */,
+       5,    0,   43,    2, 0x08 /* Private */,
+       6,    0,   44,    2, 0x08 /* Private */,
+       7,    1,   45,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Float,    4,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    8,
+
+       0        // eod
+};
+
+void Controller::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Controller *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->move(); break;
+        case 1: _t->getPoisonLevel((*reinterpret_cast< float(*)>(_a[1]))); break;
+        case 2: _t->resetPoisonLevel(); break;
+        case 3: _t->getPoisoned(); break;
+        case 4: _t->gameover((*reinterpret_cast< int(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject Controller::staticMetaObject = { {
+    QMetaObject::SuperData::link<QObject::staticMetaObject>(),
+    qt_meta_stringdata_Controller.data,
+    qt_meta_data_Controller,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *Controller::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Controller::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Controller.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int Controller::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 5)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 5;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 5)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 5;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
---
+++ build-final-Desktop_Qt_5_15_1_GCC_64bit-Debug/ui_mainwindow.h
@@ -0,0 +1,184 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.15.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QProgressBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QHBoxLayout *horizontalLayout_3;
+    QVBoxLayout *verticalLayout_2;
+    QHBoxLayout *horizontalLayout;
+    QLabel *label_2;
+    QProgressBar *energyBar;
+    QHBoxLayout *horizontalLayout_2;
+    QLabel *label_3;
+    QProgressBar *healthBar;
+    QVBoxLayout *verticalLayout;
+    QLabel *label;
+    QLineEdit *lineEdit;
+    QVBoxLayout *verticalLayout_3;
+    QSpacerItem *verticalSpacer;
+    QPushButton *changeSceneButton;
+    QPushButton *autoplayButton;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(800, 800);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        horizontalLayout_3 = new QHBoxLayout(centralwidget);
+        horizontalLayout_3->setObjectName(QString::fromUtf8("horizontalLayout_3"));
+        verticalLayout_2 = new QVBoxLayout();
+        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        label_2 = new QLabel(centralwidget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+
+        horizontalLayout->addWidget(label_2);
+
+        energyBar = new QProgressBar(centralwidget);
+        energyBar->setObjectName(QString::fromUtf8("energyBar"));
+        energyBar->setStyleSheet(QString::fromUtf8("QProgressBar{\n"
+"border: 1px solid grey;\n"
+"border-radius: 5px;\n"
+"text-align: center;\n"
+"}\n"
+"QProgressBar::chunk{\n"
+"	background-color: rgb(112, 158, 218);\n"
+"}"));
+        energyBar->setValue(24);
+
+        horizontalLayout->addWidget(energyBar);
+
+
+        verticalLayout_2->addLayout(horizontalLayout);
+
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
+        label_3 = new QLabel(centralwidget);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+
+        horizontalLayout_2->addWidget(label_3);
+
+        healthBar = new QProgressBar(centralwidget);
+        healthBar->setObjectName(QString::fromUtf8("healthBar"));
+        healthBar->setStyleSheet(QString::fromUtf8("QProgressBar{\n"
+"border: 1px solid grey;\n"
+"border-radius: 5px;\n"
+"text-align: center;\n"
+"}\n"
+"QProgressBar::chunk{\n"
+"background-color: rgb(141, 161, 98);\n"
+"}\n"
+""));
+        healthBar->setValue(24);
+
+        horizontalLayout_2->addWidget(healthBar);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_2);
+
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+
+        verticalLayout_2->addLayout(verticalLayout);
+
+        label = new QLabel(centralwidget);
+        label->setObjectName(QString::fromUtf8("label"));
+
+        verticalLayout_2->addWidget(label);
+
+        lineEdit = new QLineEdit(centralwidget);
+        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+
+        verticalLayout_2->addWidget(lineEdit);
+
+
+        horizontalLayout_3->addLayout(verticalLayout_2);
+
+        verticalLayout_3 = new QVBoxLayout();
+        verticalLayout_3->setObjectName(QString::fromUtf8("verticalLayout_3"));
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout_3->addItem(verticalSpacer);
+
+        changeSceneButton = new QPushButton(centralwidget);
+        changeSceneButton->setObjectName(QString::fromUtf8("changeSceneButton"));
+        QSizePolicy sizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(changeSceneButton->sizePolicy().hasHeightForWidth());
+        changeSceneButton->setSizePolicy(sizePolicy);
+
+        verticalLayout_3->addWidget(changeSceneButton);
+
+        autoplayButton = new QPushButton(centralwidget);
+        autoplayButton->setObjectName(QString::fromUtf8("autoplayButton"));
+
+        verticalLayout_3->addWidget(autoplayButton);
+
+
+        horizontalLayout_3->addLayout(verticalLayout_3);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 26));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        label_2->setText(QCoreApplication::translate("MainWindow", "Energy", nullptr));
+        label_3->setText(QCoreApplication::translate("MainWindow", "Health", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "TextLabel", nullptr));
+        changeSceneButton->setText(QCoreApplication::translate("MainWindow", "changeScene", nullptr));
+        autoplayButton->setText(QCoreApplication::translate("MainWindow", "Autoplay", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
